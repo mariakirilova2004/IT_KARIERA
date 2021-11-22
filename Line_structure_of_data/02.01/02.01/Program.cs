67929778491c3f9a12f0da0baa048607d4eac7bb
mariakirilova2004 <47693680+mariakirilova2004@users.noreply.github.com>
2021-11-22 10:22:02 +0200

    Add files via upload

---
+++ Line_structure_of_data/02.01/02.01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(5);
+            list[0] = list[0] + 1;
+            int element = list.RemoveAt(0);
+        }
+    }
+}
---
+++ Line_structure_of_data/02.03/02.03/SingleNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._03
+{
+    class SingleNode<T>
+    {
+        public T Value { get; set; }
+        public T Value { get; set; }
+    }
+}
