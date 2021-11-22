3ad651b4ab7cea7fdedbdffca4b4c2f45d7c8574
mariakirilova2004 <47693680+mariakirilova2004@users.noreply.github.com>
2021-11-22 10:25:06 +0200

    Add files via upload

---
+++ Train/Train/Deque.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+	public class Deque<T> : IList<T>
+	{
+		public Deque() : this(16)
+		{
+			//празен конструктор, задава капацитета на дека на стойността по подразбиране (16)
+		}
+		public Deque(int capacity)
+		{
+			//създава дека с точно зададен капацитет
+		}
+		public Deque(IEnumerable<T> collection)
+				: this(collection.Count())
+		{
+			//създава дека с капацитет съответстващ на посочената колекция и прехвърля елементите от колекцията в дека
+		}
+		public int Capacity; //показва капацитета
+		public int Count; //показва броят елементи
+		public void AddFront(T item)
+		{
+			//добавя елемент отпред
+		}
+		public void AddBack(T item)
+		{
+			//добавя елемент отзад
+		}
+		public T RemoveFront()
+		{
+			//връща и премахва елемента отпред
+		}
+		public T RemoveBack()
+		{
+			//връща и премахва елемента отзад
+		}
+		public T GetFront()
+		{
+			//връща, без да премахва, елемента отпред
+		}
+		public T GetBack()
+		{
+			//връща, без да премахва, елемента отзад
+		}
+	}
+
+}
