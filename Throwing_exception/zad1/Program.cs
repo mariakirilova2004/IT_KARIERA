411c2531e34a574438f31b9d1cdb175a73392209
mariakirilova2004 <47693680+mariakirilova2004@users.noreply.github.com>
2021-11-22 10:54:22 +0200

    Add files via upload

---
+++ Throwing_exception/zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int r = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Throwing_exception/zad2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 9; i++)
+            {
+                try
+                {
+                    int re = int.Parse(Console.ReadLine());
+                }
+                catch()
+                {
+
+                }
+            }
+        }
+        public void ReadNumber(int start, int end)
+        {
+
+        }
+    }
+}
