99dfc621cbd25c89d5e37c733f5b25cce465a551
Maria Kirilova <47693680+mariakirilova2004@users.noreply.github.com>
2023-01-21 18:06:38 +0200

    Add files via upload

---
+++ ChatServer/ChatClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            IPAddress iPAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEndPoint = new IPEndPoint(iPAddress, port: 11000);
+
+            Socket sender = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEndPoint);
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                while (true)
+                {
+                    Console.Write("> ");
+                    var message = Console.ReadLine();
+
+                    byte[] msgBytes = Encoding.ASCII.GetBytes(message + "<EOF>");
+
+                    int bytesSent = sender.Send(msgBytes);
+
+                    if(message == "exit")
+                    {
+                        break;
+                    }
+                }
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            IPAddress iPAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, port: 11000);
+
+            Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                byte[] buffer = new byte[1024];
+
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                Socket handle = listener.Accept();
+
+                while (true)
+                {
+                    string message = "";
+
+                    while (true)
+                    {
+                        var messageSize = handle.Receive(buffer);
+                        message += Encoding.ASCII.GetString(buffer, 0, messageSize);
+
+                        if (message.Contains("<EOF>"))
+                        {
+                            message = message.Replace("<EOF>", "");
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("> " + message);
+
+                    if(message == "exit")
+                    {
+                        handle.Shutdown(SocketShutdown.Both);
+                        handle.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
